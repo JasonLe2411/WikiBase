819f27c65f2f0fc66cc9070134e36b8f79d7078a
Chau Tran <chau.t.tran.us@gmail.com>
2023-03-11 15:14:00 -0500

    Added summary queries

--- GPT/main.py
+++ GPT/main.py
@@ -36,3 +36,41 @@ def init_index(datapath: Path) -> GPTListIndex:
 
     return index
 
+
+def wiki_summary() -> str:
+    args = parse_args()
+    index = init_index(args.datapath)
+    key = args.datapath.name.rstrip('/')
+
+    all_topics = index.query(
+            '''Retrieve all topics from the document if it is "sufficiently talked about". Consider
+            "sufficiently talked about" as a topic which has been talked about for 300 words or more. Return this
+            as a comma-separated list.'''
+    )
+    print('Generating topics for:', all_topics)
+
+    for topic in all_topics.response.split(','):
+        page = f'# {topic}\n'
+
+        query_str = f'''
+        Generate a summary for each entry in a table of contents of the topic '{topic}' in this text in the form:
+        ## <entry> \n
+        <summary> \n
+        \n
+        Make sure to include who spoke in the summaries.
+        '''
+
+        summary = index.query(query_str)
+        page += summary.response
+        
+        formatted_topic = topic.strip().replace(' ', '_')
+        output_path = Path('summaries').joinpath(f'{key}', f'{formatted_topic}.md')
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+        output_path.write_text(page.join(c for c in s if ord(c) < 128)) # filter out invalid characters
+
+        print(f'Created .md for {topic}')
+
+
+
+if __name__ == '__main__':
+    wiki_summary()
