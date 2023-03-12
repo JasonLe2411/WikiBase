9a1f4bc84b0371f64f72ca04d90ee995845518db
Chau Tran <chau.t.tran.us@gmail.com>
2023-03-11 22:14:37 -0500

    Section extraction and markdown templating

--- GPT/main.py
+++ GPT/main.py
@@ -3,7 +3,8 @@ from pathlib import Path
 import os
 import yaml
 
-from llama_index import SimpleDirectoryReader, GPTListIndex 
+from llama_index import SimpleDirectoryReader, GPTListIndex, LLMPredictor
+from langchain import OpenAI
 
 
 def parse_args() -> argparse.Namespace:
@@ -18,6 +19,23 @@ def init_openai_keys() -> None:
         os.environ['OPENAI_API_KEY'] = secrets['OPENAI_API_KEY']
 
 
+def init_env() -> dict[str, int]:
+    env = {}
+    with open('env.yaml', 'r') as f:
+        env_yaml = yaml.safe_load(f)
+        env['max_topics'] = env_yaml['max_topics']
+        env['max_sections'] = env_yaml['max_sections']
+    return env
+
+
+def init_llm() -> LLMPredictor:
+    temperature = 0
+    model_name = 'text-davinci-003'
+    max_tokens = 512 
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=temperature, model_name=model_name, max_tokens=max_tokens))
+    return llm_predictor
+
+
 def init_index(datapath: Path) -> GPTListIndex:
     init_openai_keys()
     key = datapath.name.rstrip('/')
@@ -25,16 +43,19 @@ def init_index(datapath: Path) -> GPTListIndex:
 
     index = None
 
+    llm = init_llm()
+
     if indexpath.exists():
         print('Loading existing vector index')
-        index = GPTListIndex.load_from_disk(indexpath)
+        index = GPTListIndex.load_from_disk(indexpath, llm_predictor=llm)
     else:
         print('Initializing new vector index...')
         documents = SimpleDirectoryReader(datapath).load_data()
-        index = GPTListIndex(documents)
+        index = GPTListIndex(documents, llm_predictor=llm)
         index.save_to_disk(indexpath)
 
     return index
+
 
 
 def wiki_summary() -> str:
@@ -42,34 +63,53 @@ def wiki_summary() -> str:
     index = init_index(args.datapath)
     key = args.datapath.name.rstrip('/')
 
-    all_topics = index.query(
-            '''Retrieve all topics from the document if it is "sufficiently talked about". Consider
-            "sufficiently talked about" as a topic which has been talked about for 300 words or more. Return this
-            as a comma-separated list.'''
-    )
-    print('Generating topics for:', all_topics)
+    env = init_env()
+    max_topics = env['max_topics']
+    max_sections = env['max_sections']
 
-    for topic in all_topics.response.split(','):
+#   all_topics = index.query(
+#           '''Retrieve all topics from the document if it is "sufficiently talked about". Consider
+#           "sufficiently talked about" as a topic which has been talked about for 300 words or more. Return this
+#           as a comma-separated list.'''
+#   ).response.split(',')
+
+    topic_pool = 'Multi-Head Attention, Scaled Dot-Product Attention, Self-Attention'.split(',')
+    topics = topic_pool[:max_topics] if len(topic_pool) >= max_topics else topic_pool
+
+    print('Generating topics for:', topics)
+    for topic in topics:
+        topic = topic.strip()
         page = f'# {topic}\n'
 
-        query_str = f'''
-        Generate a summary for each entry in a table of contents of the topic '{topic}' in this text in the form:
-        ## <entry> \n
-        <summary> \n
-        \n
-        Make sure to include who spoke in the summaries.
-        '''
+        section_query = f'''Generate subtopics about the topic "{topic}" as a comma-separated list.'''
+        section_pool = index.query(section_query).response.split(',')
+        sections = section_pool[:max_sections] if len(topic_pool) >= max_sections else section_pool
 
-        summary = index.query(query_str)
-        page += summary.response
-        
+        content_query = f'''
+Fill in the template below:\n
+## Introduction\n
+<introduction about {topic}>\n'''
+
+        for section in sections:
+            section = section.strip()
+            form = f'''
+## {section}\n
+<summary about {section}>\n'''
+            content_query += form
+
+        print(content_query)
+        content = index.query(content_query).response
+        page += content 
+
+        print(page)
+
         formatted_topic = topic.strip().replace(' ', '_')
         output_path = Path('summaries').joinpath(f'{key}', f'{formatted_topic}.md')
+
         output_path.parent.mkdir(parents=True, exist_ok=True)
-        output_path.write_text(page.join(c for c in s if ord(c) < 128)) # filter out invalid characters
+        output_path.write_text(''.join(c for c in page if ord(c) < 128)) # filter out invalid characters
 
         print(f'Created .md for {topic}')
-
 
 
 if __name__ == '__main__':
